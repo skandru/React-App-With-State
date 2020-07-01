2fb5639dbf5e2cf07a92fe6c75e356497e47bc8a
skandru <kandru.suresh@gmail.com>
2020-06-30 20:43:51 -0700

    React app with state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './components/card';
 import './App.css';
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state={
+    avatarId:5,
+    name:'Cori Dawson'
+  }
+
+  changeDetails=()=>{
+    this.setState({
+      avatarId: 45,
+      name: 'Nicole Tovar'
+    })
+  };
+
+  render(){
+    return(
+      <div className='container'>
+        <div className='row'>
+          <Card name={this.state.name} job='Teacher' avatarId={this.state.avatarId}/>
+          <Card name='David Sond' job='Playing' avatarId={2}/>
+          <Card name='Ikesha' job='Musician' avatarId={44}/>          
+        </div>
+        <div className='row'>
+          <div className='col-md-3'>
+            <a onClick={this.changeDetails} className='changedetails' href='#'>Change Details</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
